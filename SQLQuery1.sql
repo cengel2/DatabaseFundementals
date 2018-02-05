002da1e0b6daa9ae45a7a5850563a9af0e47239e
cengel2 <35233487+cengel2@users.noreply.github.com>
2018-02-05 11:51:56 -0700

    ClassNotesFeb-5-18

---
+++ SQLQuery1.sql
@@ -0,0 +1,112 @@
+        /* *************************************************
+        *Simple Tble Creation - Columns and Primary Keys
+        *
+        *Emergency Service & Product
+        *Specification Document 1
+        *Version 1.0.0
+        *
+        *Author: Dan Gilleland
+        *************************************** */
+        -- Select the CREATE DATABASE statement below to create the demo database.
+        -- CREATE DATABASE [ESB-Db-A01]
+
+        USE [ESB-Db-A01] -- this is a statement that tells us to switch to a particular database
+        --Notice in the database name above, it is wrapped in square brackets 
+        --because the name has a hyphen in it.
+        --For all our other objects(tables, columns, etc), we won't use hyphens or spaces, so
+        --the use of brackets are optional.
+
+        GO -- this statement helps to -separate- various DDL statements in out script 
+            --so that they are executed as "blocks" of code
+
+        -- To create a database table, we use the CREATE TABLE statement.
+        -- Note that the order in which we create/drop tables is important
+        -- because of how the tables are related via Forien Key.
+
+        /* DROP TABLE statements (to "clean up" the database for re-creation) */
+        IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Orders')
+            DROP TABLE Orders
+        IF EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Customers')
+            DROP TABLE Customers
+
+        Create table Customers
+        (
+            --The body of a CREATE TABLE will identify a comma-separated list of
+            --Column Declarations and Table Constraints.
+            CustomerNumber int
+				-- The following is a PRIMARY KEY constraint that has a specific name 
+				-- PK constraints ensure a row oF data being added to the table
+				-- will have to have a unique value for the PK column(s)
+				CONSTRAINT PK_Customers_CustomerNumber
+					PRIMARY KEY
+					-- IDENTITY mean the database will generate a unique whole-number
+					--value for this column
+					IDENTITY(100,1) -- THE FIRST NUMBER IS THE "SEED"
+									-- AND THE LAST NUMBER IS THE "INCREMENT"
+												NOT NULL, -- NOT NULL means the data is required
+            FirstName varchar(50)				NOT NULL,
+            LastName varchar(50)				NOT NULL,
+            Address varchar(40)					NOT NULL,
+            City varchar(35)					NOT NULL,
+            Province char(2)
+				CONSTRAINT DF_Customers_Province
+					DEFAULT ('AB')
+				CONSTRAINT CK_Customers_Province
+					CHECK  (Province = 'AB' OR
+							Province = 'BC' OR
+							Province = 'SK' OR
+							Province = 'MB' OR
+							Province = 'QC' OR
+							Province = 'ON' OR
+							Province = 'NT' OR
+							Province = 'NB' OR
+							Province = 'NL' OR
+							Province = 'YK' OR
+							Province = 'NU' OR
+							Province = 'PE')					
+												NOT NULL,
+            PostalCode char(6)
+				CONSTRAINT CK_Customers_PostalCode
+					CHECK (PostalCode LIKE '[A-Z][0-9][A-Z][0-9][A-Z][0-9]')					
+												NOT NULL,
+            PhoneNumber char(13)
+				CONSTRAINT CK_Customers_PhoneNumber
+					CHECK (PhoneNumber LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+												NULL -- NULL means data is optional.
+        )
+        CREATE TABLE Orders
+        (
+            OrderNumber	int
+				CONSTRAINT PK_Orders_OrderNumber
+					PRIMARY KEY
+					IDENTITY(200,1)
+											NOT NULL,
+            CustomerNumber int
+				--Foreign Key constraints ensure that when a row of data is being inserted or updated, 
+				--there is a row in the referenced table 
+				--that has the same value as its primary key
+			CONSTRAINT FK_Orders_CustomerNumber_Customers_CustomerNumber
+				FOREIGN KEY REFERENCES				
+				Customers(CustomerNumber)	NOT NULL,
+            [Date] datetime					NOT NULL,
+            Subtotal money
+					CONSTRAINT CK_ORDERS_Subtotal
+					CHECK (Subtotal > 0)	NOT NULL,
+            GST money
+				CONSTRAINT CK_ORDERS_GST
+				CHECK (GST >= 0)			NOT NULL,
+            Total AS Subtotal + GST						
+        )
+		
+		GO -- End of a batch of instructions
+		
+		-- Let's insert a few rows of data for the tables
+PRINT 'Inserting Customer Data'
+INSERT INTO Customers(FirstName,LastName,[Address], City, Province, PostalCode)
+	VALUES('Clark', 'Kent', '344 Clinton Street', 'Metropolis', 'DE', 'S0S0N0')
+INSERT INTO Customers(FirstName,LastName,[Address], City, Province, PostalCode)
+	VALUES('Jimmy', 'Olson', '242 River Close', 'Bakerline', 'DE', 'B4K3R1')
+
+	PRINT 'Inserting an order'
+	INSERT INTO Orders(CustomerNumber, [Date], Subtotal, GST, Total)
+		VALUES (100, GETDATE(), 17.45, 0.87, 20.00)
